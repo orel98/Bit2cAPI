239696f2c93de64f2b55fb4845ca4c66fb994dd6
orel98 <orel98@hotmail.co.il>
2022-03-23 18:35:25 +0200

    update client

--- api_client.py
+++ api_client.py
@@ -1,72 +1,55 @@
-from enum import Enum
 import math
-import time
-from typing import Any
 import requests
 import hmac
 import hashlib
 import base64
 import json
-from datetime import datetime, timedelta
+from datetime import datetime
 from json.decoder import JSONDecodeError
-from requests.exceptions import ConnectionError
 
 
-class Symbol(Enum):
-
-    BTC = "BTC"
-    ETH = "ETH"
-    LTC = "LTC"
-    USDC = "USDC"
-
-    @property
-    def pair(self):
-        return f"{self.value.capitalize()}Nis"
+from Symbol import Symbol
 
 
 class Bit2c_client:
     """class for accessing private methods of bit2c.co.il api"""
 
     def __init__(self, key: str, secret: str) -> None:
-        self.key = key
-        self.secret = secret
+        self.__key = key
+        self.__secret = secret
 
     def create_hash(self, hash_key: str) -> bytes:
         sign = base64.b64encode(
-            hmac.new(self.secret.encode(), hash_key.encode(), hashlib.sha512).digest()
+            hmac.new(self.__secret.encode(), hash_key.encode(), hashlib.sha512).digest()
         )
         return sign
 
     def query(self, url: str, method: str, data: dict = {}) -> requests.Response:
         BASE_URL = "https://bit2c.co.il/"
         # params to hash should be param1=x&param2=y...&nonce=number
-        parmas_string = ""
-        nonce = self.nonce
-        print(f"{nonce=}")
+        params_string = ""
         for item in data.items():
-            parmas_string += "=".join([str(i) for i in list(item)]) + "&"
-        parmas_string += f"nonce={nonce}"
+            params_string += "=".join([str(i) for i in list(item)]) + "&"
+
+        nonce = self.nonce
+        params_string += f"{nonce=}"
 
         url = BASE_URL + url
-        sign = self.create_hash(parmas_string)
+        sign = self.create_hash(params_string)
         headers = {
-            "Key": self.key,
+            "Key": self.__key,
             "Sign": sign,
             "Content-Type": "application/x-www-form-urlencoded",
         }
 
         if method == "GET":
-            url = f"{url}?{parmas_string}"
-
-            res = requests.get(url, headers=headers)
-            print(res.status_code)
-            return res
+            url = f"{url}?{params_string}"
+            return requests.get(url, headers=headers)
 
         if method == "POST":
             data["nonce"] = nonce
             return requests.post(url, headers=headers, data=data)
 
-    # @classmethod
     @property
     @staticmethod
     def nonce(*args):
@@ -139,28 +122,24 @@ class Bit2c_client:
             "Pair": symbol.pair,
         }
         res = self.query(url, "POST", data)
-        self.LOG.write_to_log(res.text)
         return self._json(res.text)
 
     def cancel_order(self, id: int) -> str:
         url = "Order/CancelOrder"
         data = {"id": id}
         res = self.query(url, "POST", data)
-        self.LOG.write_to_log(res.text)
         return self._json(res.text)
 
     def buy_market_price(self, total: float, symbol: Symbol) -> str:
         url = "Order/AddOrderMarketPriceBuy"
         data = {"Total": total, "Pair": symbol.pair}
         res = self.query(url, "POST", data)
-        self.LOG.write_to_log(res.text)
         return self._json(res.text)
 
     def sell_market_price(self, amount: float, pair: float) -> str:
         url = "Order/AddOrderMarketPriceSell"
         data = {"Amount": amount, "Pair": pair}
         res = self.query(url, "POST", data)
-        self.LOG.write_to_log(res.text)
         return self._json(res.text)
 
     def add_stop_limit_order(
@@ -175,15 +154,4 @@ class Bit2c_client:
             "Pair": symbol.pair,
         }
         res = self.query(url, "POST", data)
-        self.LOG.write_to_log(res.text)
         return self._json(res.text)
-
-
-if __name__ == "__main__":
-    import os
-
-    key = os.getenv("BIT2CKEY")
-    secret = os.getenv("BIT2CSECRET")
-    # print(key, secret)
-    api = Bit2c_client(key, secret)
-    print(api.fetch_balance())
