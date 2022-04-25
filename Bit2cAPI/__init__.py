840ce531cf288770f67fbe866958a2a2c7f4e39b
orel98 <orel98@hotmail.co.il>
2022-04-25 15:39:43 +0300

    update

--- Bit2cAPI/__init__.py
+++ Bit2cAPI/__init__.py
@@ -1,2 +1,8 @@
 from .api_client import Bit2c_client
 from .Symbol import Symbol
+
+import requests
+
+# This is very important
+# bit2c api knowns to work only with ip v4, this is how we force requests to use it
+requests.packages.urllib3.util.connection.HAS_IPV6 = False
